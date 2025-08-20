4d49726a10ce2bc504cfb69fe8c4906912e81927
Colingdal <114384543+Colingdal@users.noreply.github.com>
2025-08-20 08:44:49 +0200

    new

---
+++ scripts/create-app.js
@@ -0,0 +1,12 @@
+// Getting started
+// To create a new app:
+
+// npx @sitevision/create-sitevision-app my-new-app
+// npx is a package runner tool (useful when you don't want to pollute global installations)
+
+// The command will create a directory with a basic project structure (you will be prompted to enter if you are creating a WebApp or RESTApp as well as information about your Sitevision environment).
+
+// The project will depend on @sitevision/sitevision-scripts, which gives the project access to utility scripts for building and deployment.
+
+// Version 3.0.0
+// You might run in to issues creating a new app if you have an old version of @sitevision/create-sitevision-app installed globally. To fix this, run: npx @sitevision/create-sitevision-app@3 your-app to update to the latest version.
